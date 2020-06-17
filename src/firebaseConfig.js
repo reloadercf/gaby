060cd9e4ede1cc9240b092fe0ccf712112379213
Gaby Alvarez <gabyalvarzb@gamil.com>
2020-06-17 12:47:45 -0500

    config firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCi14pjtD6VjCzGvMc63alrOehkbLrH-vg",
+    authDomain: "developmentg-31f9a.firebaseapp.com",
+    databaseURL: "https://developmentg-31f9a.firebaseio.com",
+    projectId: "developmentg-31f9a",
+    storageBucket: "developmentg-31f9a.appspot.com",
+    messagingSenderId: "64892017795",
+    appId: "1:64892017795:web:1dcb5ae4b0c9bfe5b04fb6"
+  };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebaseConfig  from './firebaseConfig';
+
+import {
+  FirebaseAppProvider
+} from 'reactfire'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  (
+  <FirebaseAppProvider firebaseConfig={ firebaseConfig }>
+  <Suspense fallback={<p>Cargando...</p>}>
+  <App />
+  </Suspense>
+  </FirebaseAppProvider>
+  ), document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
